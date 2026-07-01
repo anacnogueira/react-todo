e3b633749d9a6763b7603c27331544df8c626a95
Ana Claudia <anacnogueira@gmail.com>
2026-07-01 15:28:31 -0300

    Criando compoente Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import Icon from "./components/icon";
 
 export default function App() {
   
   return (
-    <div className="text-green-base">
-       <Text as="div" variant="body-sm-bold" className="text-pink-base">Olá mundo!</Text>
-       <Text as="div" className="text-green-base">Olá mundo!</Text>
-       <Text as="div" variant="body-md-bold">Olá mundo!</Text>
-       <Text>Levar o dog pra passear</Text>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-3">
+        <Text as="div" variant="body-sm-bold" className="text-pink-base">Olá mundo!</Text>
+        <Text as="div" className="text-green-base">Olá mundo!</Text>
+        <Text as="div" variant="body-md-bold">Olá mundo!</Text>
+        <Text>Levar o dog pra passear</Text>
+      </div>
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base" />
+      </div>
     </div>
   )
 }
