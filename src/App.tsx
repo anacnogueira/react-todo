95b4dee71f6e77bfecb6acce2f7f70c511ca1c5a
Ana Claudia <anacnogueira@gmail.com>
2026-07-01 14:06:58 -0300

    Criando compoente Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Text from "./components/text";
 
 export default function App() {
   
   return (
     <div className="text-green-base">
-      Hello World
+       <Text as="div" variant="body-sm-bold" className="text-pink-base">Olá mundo!</Text>
+       <Text as="div" className="text-green-base">Olá mundo!</Text>
+       <Text as="div" variant="body-md-bold">Olá mundo!</Text>
+       <Text>Levar o dog pra passear</Text>
     </div>
   )
 }
