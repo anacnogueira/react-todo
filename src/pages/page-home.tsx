2e996d2393b7a315fb291aa21d8c4e386a4acd1c
Ana Claudia <anacnogueira@gmail.com>
2026-07-03 11:44:26 -0300

    Criando componete de resumo das tarefas

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,5 +1,13 @@
 import Container from "../components/container";
+import TasksSummary from "../cor-components/tasks-summary";
 
 export default function PageHome() {
-    return <Container>Pàgina Home</Container>
+    return (
+        <Container as="article" className="space-y-3">
+            <header className="flex items-center justify-between">
+                <TasksSummary />
+            </header>
+            
+        </Container>
+    );
 }
